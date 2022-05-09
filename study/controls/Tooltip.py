22738220ad553583badea5e5a19da489c1bf6fdd
hxx <3522709323@qq.com>
2022-05-09 09:15:52 +0800

    2022-5-9

---
+++ study/controls/Tooltip.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QApplication, QToolTip, QPushButton, QWidget
+from PyQt5.QtGui import QFont
+
+
+class TooltipForm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        QToolTip.setFont(QFont('SanSerif'))
+        self.setToolTip('今天是<b>星期五<b>')
+        self.setGeometry(200, 200, 700, 700)
+        self.setWindowTitle('设置控件提示消息')
+
+        self.button1 = QPushButton("我的按钮")
+        self.button1.setToolTip("这是一个按钮,Are you ok?")
+        layout = QHBoxLayout()
+        layout.addWidget(self.button1)
+
+        mainFrame = QWidget()
+        mainFrame.setLayout(layout)
+        self.setCentralWidget(mainFrame)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = TooltipForm()
+    main.show()
+
+    sys.exit(app.exec_())
